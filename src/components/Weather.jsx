3d4da7cc020f286f787d6dc746920dbcd087ece6
Ritik <ritikrajput543678@gmail.com>
2026-03-24 08:40:50 +0530

    fix duplicate

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -97,25 +97,6 @@ const Weather = () => {
         </div>
       </>:<></> }
 
-      <img src={weatherData.icon} alt="" className='weather-icon' />
-      <p className='temperature'>{weatherData.temperature}°</p>
-      <p className='location'>{weatherData.location}</p>
-      <div className="weather-data">
-        <div className='col'>
-          <img src={humidity_icon} alt='' />
-          <div>
-            <p>{weatherData.humidity} %</p>
-            <span>Humidity</span>
-          </div>
-        </div>
-        <div className='col'>
-          <img src={wind_icon} alt='' />
-          <div>
-            <p>{weatherData.windSpeed} km/h</p>
-            <span>Wind Speed</span>
-          </div>
-        </div>
-      </div>
 
     </div>
   )
